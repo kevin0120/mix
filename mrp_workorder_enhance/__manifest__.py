15783ae9045b26943b1f14ed6cf7e63223caf369
gubin <gubin@cloudahead.net>
2018-04-12 20:49:07 +0800

    1.修正了依赖

--- mrp_workorder_enhance/__manifest__.py
+++ mrp_workorder_enhance/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['mrp_workorder'],
+    'depends': ['mrp_workorder', 'mrp_enhance'],
 
     # always loaded
     'data': [
