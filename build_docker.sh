49f3dd68fc841aa86951e6b3ffd396f84cb0095b
linshenqi <linshenqi@outlook.com>
2018-04-17 22:48:30 +0800

    1. 更换aliyun杭州节点

--- build_docker.sh
+++ build_docker.sh
@@ -4,7 +4,9 @@ while read -r line; do declare  $line; done < ./version.env
 _tag=`echo $line|awk -F '=' '{print $2}'`
 echo $_tag;
 
-docker build -t registry.us-west-1.aliyuncs.com/us/docker:rush_${_tag} -t registry.us-west-1.aliyuncs.com/us/docker:rush_latest .
+docker_repo="registry.cn-hangzhou.aliyuncs.com"
 
-docker push registry.us-west-1.aliyuncs.com/us/docker:rush_${_tag}
-docker push registry.us-west-1.aliyuncs.com/us/docker:rush_latest
+docker build -t ${docker_repo}:rush_${_tag} -t ${docker_repo}:rush_latest .
+
+docker push ${docker_repo}:rush_${_tag}
+docker push ${docker_repo}:rush_latest
