828a45c328c30636996a24bad2c238dafaff7513
linshenqi <linshenqi@outlook.com>
2018-04-23 09:47:54 +0800

    1. 更新了容器脚本

--- build_docker.sh
+++ build_docker.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-while read -r line; do declare  $line; done < ./version.env
-_tag=`echo $line|awk -F '=' '{print $2}'`
-echo $_tag;
+version="0.0.1"
 
-docker_repo="registry.cn-hangzhou.aliyuncs.com"
+docker_repo="registry.cn-hangzhou.aliyuncs.com/ca/docker"
 
-docker build -t ${docker_repo}:rush_${_tag} -t ${docker_repo}:rush_latest .
+docker build -t ${docker_repo}:rush_${version} -t ${docker_repo}:rush_latest .
 
-docker push ${docker_repo}:rush_${_tag}
+docker push ${docker_repo}:rush_${version}
 docker push ${docker_repo}:rush_latest
