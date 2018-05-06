8143c239adf5469c7fe1b705a06e45a9d90b5992
masami10 <masami.nagasawa10@hotmail.com>
2018-05-06 23:20:54 +0800

    1.和对象存储对接获取数据 2.修改了关于spc的模型，波形存储为列表形式

--- spc/models/result.py
+++ spc/models/result.py
@@ -5,6 +5,7 @@ from odoo.exceptions import ValidationError
 import odoo.addons.decimal_precision as dp
 from datetime import datetime, timedelta, date
 from dateutil.relativedelta import relativedelta
+import json
 
 
 class OperationResult(models.HyperModel):
@@ -13,6 +14,8 @@ class OperationResult(models.HyperModel):
     workorder_id = fields.Many2one('mrp.workorder', 'Operation')
     workcenter_id = fields.Many2one('mrp.workcenter', related='workorder_id.workcenter_id', store=True, readonly=True)  # TDE: necessary ?
     production_id = fields.Many2one('mrp.production', 'Production Order')
+
+    pset_configuration = fields.Char(string='Pset Configuration')
 
     pset_strategy = fields.Selection([('AD', 'Torque tightening'),
                                          ('AW', 'Angle tightening'),
@@ -37,7 +40,7 @@ class OperationResult(models.HyperModel):
 
     pset_w_target = fields.Float(string='Set Target Angel(grad)', digits=dp.get_precision('Quality Tests'))
 
-    cur_file = fields.Char(string='Waveform File')
+    cur_objects = fields.Char(string='Waveform Files')
 
     name = fields.Char('Name', default=lambda self: _('New'))
     point_id = fields.Many2one('quality.point', 'Control Point')
@@ -80,6 +83,16 @@ class OperationResult(models.HyperModel):
         ('fail', 'Failed')], string='Final Pass', default='fail',
                                 compute='_compute_result_pass', store=True)
 
+    @api.one
+    @api.constrains('cur_objects')
+    def _constraint_curs(self):
+        try:
+            obj = json.loads(self.cur_objects)
+            if not isinstance(list, obj):
+                raise ValidationError('Waveform File is not a list')
+        except ValueError:
+            raise ValidationError('Waveform File is not Schema correct')
+
     @api.multi
     @api.depends('measure_result', 'op_time')
     def _compute_result_pass(self):
@@ -113,6 +126,14 @@ class OperationResult(models.HyperModel):
         if 'name' not in vals or vals['name'] == _('New'):
             vals['name'] = self.env['ir.sequence'].next_by_code('quality.check') or _('New')
         return super(OperationResult, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if 'cur_objects' in vals:
+            cur_objects = list(set(json.loads(vals['cur_objects'])))  # 去重
+            vals.update({'cur_objects': json.dumps(cur_objects)})
+        ret = super(OperationResult, self).write(vals)
+        return ret
 
     @api.multi
     def do_fail(self):
--- wave/models/wave.py
+++ wave/models/wave.py
@@ -3,6 +3,11 @@
 from odoo import models, fields, api
 from pyecharts import Bar
 import pyecharts
+from minio import Minio
+from urlparse import urlsplit
+import json
+
+minioClient = None
 
 
 class Wave(models.TransientModel):
@@ -20,6 +25,21 @@ class Wave(models.TransientModel):
 
     echart = fields.Text(string='EChart', store=False)
 
+    def _recreate_minio_client(self):
+        global minioClient
+        minio_url = self.env['ir.config_parameter'].get_param('minio.url')
+        minio_bucket = self.env["ir.config_parameter"].get_param("minio.bucket")
+        minio_access_key = self.env["ir.config_parameter"].get_param("minio.access_key")
+        minio_secret_key = self.env["ir.config_parameter"].get_param("minio.secret_key")
+        secruity = False if urlsplit(minio_url).scheme == 'http' else True
+        if not minioClient:
+            minioClient = Minio(minio_url.split('://')[-1], access_key=minio_access_key, secret_key=minio_secret_key, secure=secruity)
+        elif minioClient._endpoint_url != minio_url or minioClient._access_key != minio_access_key or minioClient._secret_key != minio_secret_key:
+            minioClient = Minio(minio_url.split('://')[-1], access_key=minio_access_key, secret_key=minio_secret_key, secure=secruity)
+        if not minioClient.bucket_exists(minio_bucket):
+            minioClient.make_bucket(minio_bucket)
+        return minioClient, minio_bucket
+
     def _get_echart_data(self):
         bar = Bar("我的第一个图表", "这里是副标题")
         bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
@@ -31,8 +51,15 @@ class Wave(models.TransientModel):
     def read(self, fields=None, load='_classic_read'):
         result = super(Wave, self).read(fields, load=load)
         if 'echart' in fields and load == '_classic_read':
+            data = self._get_data()
             result[0].update({'echart': self._get_echart_data()})
         return result
+
+    def _get_data(self):
+        client, bucket = self._recreate_minio_client()
+        stream_data = client.get_object(bucket, 'test.json')
+        data = json.loads(stream_data.data.decode('utf-8'))
+        return data
 
     @api.multi
     def button_query(self):
