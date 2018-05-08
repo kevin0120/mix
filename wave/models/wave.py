a085456fd5b79e134401fb616dd29211c77a90eb
masami10 <masami.nagasawa10@hotmail.com>
2018-05-08 18:10:48 +0800

    模拟测试波形数据通过pandas进行数据整理通过echarts渲染可视化

--- wave/models/wave.py
+++ wave/models/wave.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from pyecharts import Bar
+from pyecharts import Line
 import pyecharts
 from minio import Minio
 from urlparse import urlsplit
 import json
+from pandas import DataFrame
 
 minioClient = None
+
+
+def _create_wave_result_dict(x,data):
+    _ret = dict()
+    _ret['name'] = x
+    _data = json.loads(data)
+    _ret['result'] = _data['result']  # 不是dataframe 对象
+    _data.pop('result')
+    _data[u'tor_{0}'.format(x)] = _data['cur_m']
+    _data.pop('cur_m')
+    _ret['wave'] = DataFrame.from_dict(_data)
+
+    return _ret
 
 
 class Wave(models.TransientModel):
@@ -46,28 +60,32 @@ class Wave(models.TransientModel):
             minioClient.make_bucket(minio_bucket)
         return minioClient, minio_bucket
 
-    def _get_echart_data(self,datas):
-        bar = Bar("我的第一个图表", "这里是副标题")
-        bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
-        bar.print_echarts_options()
+    def _get_echart_data(self,datas,ret):
+        line = Line(u"{0}".format(u'Waveform'), u"{0}-{1}".format(self.query_date_from, self.query_date_to), width=1920,height=1080)
+        for index, data in enumerate(datas):
+            line.add(data['name'], ret['cur_w'].values, ret[u'tor_{0}'.format(data['name'])].values, is_smooth=True, is_more_utils=True)
+            # line.options.get('series')[index].update({'connectNulls': True})
+        # line.print_echarts_options()
         pyecharts.configure(force_js_embed=True)
-        return bar.render_embed()
+        return line.render_embed()
 
     @api.multi
     def read(self, fields=None, load='_classic_read'):
         result = super(Wave, self).read(fields, load=load)
         if 'wave' in fields and load == '_classic_read':
-            datas = self._get_data()
-            result[0].update({'wave': self._get_echart_data(datas)})
+            datas,ret = self._get_data()
+            result[0].update({'wave': self._get_echart_data(datas,ret)})
         return result
 
     def _get_data(self):
         client, bucket = self._recreate_minio_client()
-        ret = []
-        for i in range(2):
-            stream_data = client.get_object(bucket, 'cur.json')
-            data = json.loads(stream_data.data.decode('utf-8'))
-        return data
+        objects = ['cur1.json', 'cur2.json', 'cur3.json', 'cur4.json', 'cur5.json']
+        ret = None
+        _datas = map(lambda x: _create_wave_result_dict(x, client.get_object(bucket, x).data.decode('utf-8')), objects)
+        for i in range(len(_datas) -1):
+            ret = _datas[i]['wave'].merge(_datas[i+1]['wave'],how='outer', on='cur_w') if i == 0 else ret.merge(_datas[i+1]['wave'],how='outer', on='cur_w')
+        ret = ret.sort_values(by=['cur_w'])
+        return _datas,ret
 
     @api.multi
     def button_query(self):
