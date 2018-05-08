bff7075445c0973c2d107e72ca2f7fdd73915550
gubin <gubin@empower.cn>
2018-05-08 20:42:12 +0800

    update

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 
 set -e
 BUILD_PATH=`pwd`
-docker build --build-arg CODING_PWD=$CODING_PWD CODING_USER=$CODING_USER GITHUB_PWD=$GITHUB_PWD GITHUB_USER=$GITHUB_USER -f ./Dockerfile -t gubinempower/sa-backend:20180508 . --no-cache
+docker build --build-arg CODING_PWD=$CODING_PWD --build-arg CODING_USER=$CODING_USER --build-arg GITHUB_PWD=$GITHUB_PWD --build-arg GITHUB_USER=$GITHUB_USER -f ./Dockerfile -t gubinempower/sa-backend:20180508 . --no-cache
 exit 0
