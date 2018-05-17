22794b515b7c1c23a7400a617b5ff868c11641cd
masami10 <masami.nagasawa10@hotmail.com>
2018-05-17 13:34:32 +0800

    1.第一版SPC

--- spc/models/res_config.py
+++ spc/models/res_config.py
@@ -21,3 +21,32 @@ class AiisSettings(models.TransientModel):
         self.env['ir.config_parameter'].set_param("aiis.url",
                                                   self.aiis_url or 'http://127.0.0.1:9000',
                                                   groups=['base.group_system'])
+
+
+class SPCSetting(models.TransientModel):
+    _name = 'spc.config.settings'
+    _inherit = 'res.config.settings'
+
+    scale_parameter = fields.Float('Weibull Distribution Scale Parameter', default=1.0)
+
+    shape_parameter = fields.Float('Weibull Distribution Shape Parameter', default=5.0)
+
+    def get_default_all(self, fields):
+        scale_parameter = self.env["ir.config_parameter"].get_param("weibull.scale", default=1.0)
+
+        shape_parameter = self.env["ir.config_parameter"].get_param("weibull.shape", default=5.0)
+
+        return dict(
+            scale_parameter=scale_parameter,
+            shape_parameter=shape_parameter
+        )
+
+    def set_scale_parameter(self):
+        self.env['ir.config_parameter'].set_param("weibull.scale",
+                                                  self.scale_parameter or 1.0,
+                                                  groups=['spc.group_spc_user'])
+
+    def set_shape_parameter(self):
+        self.env['ir.config_parameter'].set_param("weibull.shape",
+                                                  self.shape_parameter or 5.0,
+                                                  groups=['spc.group_spc_user'])
--- spc/models/tor_ang_spc.py
+++ spc/models/tor_ang_spc.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-from scipy.stats import norm
+from scipy.stats import norm, dweibull, weibull_max, weibull_min, invweibull,exponweib
 from odoo import models, fields, api
 from pyecharts import Overlap, Bar, Line, Grid
 import pyecharts
 from pandas import DataFrame
-
+import numpy as np
+from odoo.tools import float_round
 
 class TorAngSPCReport(models.TransientModel):
     _name = "ta.spc.wizard"
@@ -20,50 +21,108 @@ class TorAngSPCReport(models.TransientModel):
 
     normal_dist = fields.Text(string='Normal Distribution', store=False)
     weibull_dist = fields.Text(string='Weibull Distribution', store=False)
+    weibull_dist_method = fields.Selection([('double', 'Double Weibull'),('inverted', 'Inverted'), ('exponential', 'Exponential'),('min', 'Min Weibull'), ('max', 'Max Weibull')], string='韦伯分布统计方法', default='min')
     scatter = fields.Text(string='Scatter', store=False)
 
-    def _get_normal_dist(self,data):
-        grid = Grid(width=1920, height=1080)
-        attr = ["{}月".format(i) for i in range(1, 13)]
-        v1 = [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-        v2 = [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-        v3 = [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+    def _get_weibull_dist(self, qty, mean=None, std=None, scale=1.0, shape=5.0):
 
-        bar = Bar(title=u"Normal Distribution({0})".format(self.spc_target), title_pos="50%")
-        bar.add("蒸发量", attr, v1)
-        bar.add("降水量", attr, v2, yaxis_formatter=" ml", yaxis_max=250,
-                legend_pos="85%", legend_orient="vertical", legend_top="45%")
-        line = Line()
-        line.add("平均温度", attr, v3, yaxis_formatter=" °C", is_smooth=True )
-        overlap = Overlap(width=1920, height=1080)
-        overlap.add(bar)
-        overlap.add(line, is_add_yaxis=True, yaxis_index=1)
+        x_line = np.arange(mean - std * 4.0, mean + std * 5.0, 1 * std)
 
-        grid.add(overlap)
+        if self.weibull_dist_method == 'double':
+            _data = dweibull(shape, loc=mean, scale=std)
+            y_line = _data.pdf(x_line) * qty
+
+        if self.weibull_dist_method == 'inverted':
+            _data = invweibull(shape, loc=mean, scale=std)
+            y_line = _data.pdf(x_line) * qty
+
+        if self.weibull_dist_method == 'exponential':
+            _data = exponweib(scale, shape, loc=mean, scale=std)
+            y_line = _data.pdf(x_line) * qty
+
+        if self.weibull_dist_method == 'min':
+            _data = weibull_min(shape, loc=mean, scale=std)
+            y_line = _data.pdf(x_line) * qty
+
+        if self.weibull_dist_method == 'max':
+            _data = weibull_max(shape, loc=mean, scale=std)
+            y_line = _data.pdf(x_line) * qty
+
+        line = Line(width=1280, height=600)
+        line.add(u'{0}'.format(self.spc_target), x_line, y_line, xaxis_name=u'{0}'.format(self.spc_target),
+                 yaxis_name=u'数量(Quantity)',
+                 line_color='rgba(0 ,255 ,127,0.5)', legend_pos='center',
+                 is_smooth=True, line_width=2,
+                 tooltip_tragger='axis', is_fill=True, area_color='#20B2AA', area_opacity=0.4)
+        pyecharts.configure(force_js_embed=True)
+        return line.render_embed()
+
+    def _get_normal_dist(self, mean=None, std=None):
+        norm_data = norm(mean, std)
+        x_bar = np.arange(mean - std * 3.0, mean + std * 4.0, 1 * std)
+        y_bar = norm_data.pdf(x_bar)
+
+        x_line = np.arange(mean - std * 3.0, mean + std * 4.0, 1 * std)
+        y_line = norm_data.pdf(x_line)
+
+        bar = Bar(title=u"Normal Distribution({0})".format(self.spc_target), title_pos="50%", width=960, height=1440)
+        bar.add(u'{0}'.format(self.spc_target), x_bar, y_bar, legend_orient="vertical", legend_top="45%", legend_pos='50%',
+                xaxis_name=u'{0}'.format(self.spc_target), mark_line=["min", "max"], yaxis_name_gap=100,
+                label_color=['rgba(255,106,106,0.5)'],
+                yaxis_name=u'概率(Probability)')
+        line = Line(width=960, height=1440)
+        line.add(u'{0}'.format(self.spc_target).format(self.spc_target), x_line, y_line, xaxis_name=u'{0}'.format(self.spc_target),
+                 yaxis_name=u'概率(Probability)',
+                 line_color='rgba(0 ,255 ,127,0.5)', is_legend_show=False,
+                 is_smooth=True, line_width=2,
+                 is_datazoom_show=True, datazoom_type='both',
+                 tooltip_tragger='axis',is_fill=True, area_color='#20B2AA', area_opacity=0.4)
+
+        grid = Grid(width=1920, height=1440)
+        grid.add(bar, grid_bottom="60%", grid_left="60%")
+        grid.add(line, grid_bottom="60%", grid_right="60%")
         pyecharts.configure(force_js_embed=True)
         return grid.render_embed()
-        # bar = Bar(u'Normal Distribution', title_pos='center', width=1920,height=1080)
-        # style = Style()
-        # pie_style = style.add(label_pos="center", is_label_show=True, label_text_color=None)
-        #
-        # bar.add("", ["剧情", "xx"], [25, 75], center=[10, 30],
-        #         radius=[18, 24], legend_pos="left", legend_orient='vertical', **pie_style)
-        # bar.add("", ["奇幻", "yy"], [24, 76], center=[30, 30],
-        #         radius=[18, 24],  legend_pos="right", legend_orient='vertical', **pie_style)
-        # pyecharts.configure(force_js_embed=True)
-        # return bar.render_embed()
+
+    def _get_scatter(self,  data):
+        qty = len(data)
+        x_line = np.arange(1, qty + 1, 1)
+        y_line = data
+        line = Line(width=1280, height=800)
+        line.add(u'{0}'.format(self.spc_target), x_line, y_line, xaxis_name=u'Sequence',
+                 yaxis_name=u'{0}'.format(self.spc_target), mark_line=["min", "average", "max"],
+                 line_color='rgba(0 ,255 ,127,0.5)', legend_pos='center',
+                 is_smooth=True, line_width=2, is_more_utils=True,
+                 tooltip_tragger='axis')
+        pyecharts.configure(force_js_embed=True)
+        return line.render_embed()
 
     @api.multi
     def read(self, fields=None, load='_classic_read'):
+        data = DataFrame()
+        mean = 0.0
+        std = 0.0
         result = super(TorAngSPCReport, self).read(fields, load=load)
-        if 'normal_dist' in fields and load == '_classic_read':
+        if 'normal_dist' in fields or 'weibull_dist' in fields or'scatter' in fields and load == '_classic_read':
             data = self._get_data()
-            result[0].update({'normal_dist': self._get_normal_dist(data)})
+            mean = np.mean(data)
+            std = np.std(data)
+        if 'normal_dist' in fields and not data.empty:
+            result[0].update({'normal_dist': self._get_normal_dist(mean=mean,std=std)})
+        if 'weibull_dist' in fields and not data.empty :
+            scale_parameter = self.env['ir.config_parameter'].sudo().get_param('weibull.scale', default=1.0)
+            shape_parameter = self.env['ir.config_parameter'].sudo().get_param('weibull.shape', default=5.0)
+            result[0].update({'weibull_dist': self._get_weibull_dist(len(data), mean=mean, std=std,
+                                                                     scale=scale_parameter, shape=shape_parameter)})
+        if 'scatter' in fields and not data.empty:
+            result[0].update({'scatter': self._get_scatter(data)})
+
         return result
 
     def _get_data(self):
         domain = [('measure_result', 'in', ['ok', 'nok'])]
         fields = ['measure_torque']
+        order = 'measure_torque desc'
         if self.query_date_from:
             domain += [('control_date', '>=', self.query_date_from)]
         if self.query_date_to:
@@ -78,7 +137,11 @@ class TorAngSPCReport(models.TransientModel):
             fields = ['measure_torque']
         if self.spc_target == 'angle':
             fields = ['measure_degree']
-        return self.env['operation.result'].sudo().search_read(domain=domain, fields=fields, limit=self.limit)
+            order = 'measure_degree desc'
+        _data = self.env['operation.result'].sudo().search_read(domain=domain, fields=fields, limit=self.limit, order=order)
+        df = DataFrame.from_dict(_data)
+        df = df['measure_degree'] if self.spc_target == 'angle' else df['measure_torque']
+        return df
 
     # @api.multi
     # def button_query_vehicle(self):
