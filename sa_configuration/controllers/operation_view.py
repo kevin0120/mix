bc15e29fa1d3bdb31bc6aa1fc742cbe8804b3766
masami10 <masami.nagasawa10@hotmail.com>
2018-07-23 10:44:48 +0800

    1.获取清单时 point也无需显示

--- sa_configuration/controllers/operation_view.py
+++ sa_configuration/controllers/operation_view.py
@@ -114,7 +114,7 @@ class OperationView(http.Controller):
                     'id': operation.id,
                     'name': u"[{0}]{1}@{2}/{3}".format(operation.name, operation.group_id.code, operation.workcenter_id.name, operation.routing_id.name),
                     # "img": u'data:{0};base64,{1}'.format('image/png', operation.worksheet_img) if operation.worksheet_img else "",
-                    "points": _points
+                    # "points": _points
                 })
             body = json.dumps(vals)
             headers = [('Content-Type', 'application/json'), ('Content-Length', len(body))]
