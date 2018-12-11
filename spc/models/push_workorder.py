c0a5089777e8c0d938992e472be288908251e079
linshenqi <linshenqi@outlook.com>
2018-12-11 18:04:33 +0800

    1. 增加了下发工单数量配置 2. 放开下发作业功能限制

--- spc/models/push_workorder.py
+++ spc/models/push_workorder.py
@@ -64,9 +64,9 @@ class PushWorkorder(AbstractModel):
                 'hmi': {'id': order.workcenter_id.hmi_id.id, 'uuid': order.workcenter_id.hmi_id.serial_no} if order.workcenter_id else None,
                 'workcenter': {'name': order.workcenter_id.name,
                                'code': order.workcenter_id.code} if order.workcenter_id else None,
-                'worksheet': u'data:{0};base64,{1}'.format('image/png', order.operation_id.worksheet_img) if order.operation_id.worksheet_img else "",
-                'vehicleTypeImg': u'data:{0};base64,{1}'.format('image/png',
-                                                                order.product_id.image_small) if order.product_id.image_small else "",
+                'img_op_id': order.operation_id.id,
+                # 'worksheet': u'data:{0};base64,{1}'.format('image/png', order.operation_id.worksheet_img) if order.operation_id.worksheet_img else "",
+                # 'vehicleTypeImg': u'data:{0};base64,{1}'.format('image/png', order.product_id.image_small) if order.product_id.image_small else "",
                 'max_op_time': order.operation_id.max_op_time,
                 'vin': order.production_id.vin,
                 'knr': order.production_id.knr,
@@ -99,7 +99,8 @@ class PushWorkorder(AbstractModel):
     @api.multi
     def workerorder_push(self):
         domain = [('sent', '=', False)]
-        orders = self.env['mrp.workorder'].sudo().search(domain, limit=ORDER_LIMIT, order=ORDER_ORDER_BY)
+        limit = self.env['ir.config_parameter'].sudo().get_param('sa.wo.push.num', default=80)
+        orders = self.env['mrp.workorder'].sudo().search(domain, limit=int(limit), order=ORDER_ORDER_BY)
         masterpcs = orders.mapped('workcenter_id.masterpc_id')
         for master in masterpcs:
             need_send_orders = orders.filtered(lambda r: r.workcenter_id.masterpc_id.id == master.id)
