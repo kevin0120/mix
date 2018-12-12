7de354a8d97f7ee751b0122294f49f44bec2b773
gubin <gubin@empower.cn>
2018-12-12 09:27:21 +0800

    1.添加批量下发作业功能

--- sa_base/models/mrp_routing.py
+++ sa_base/models/mrp_routing.py
@@ -6,7 +6,7 @@ from odoo.exceptions import UserError
 
 import requests as Requests
 
-from requests import ConnectionError, RequestException
+from requests import ConnectionError, RequestException, exceptions
 
 import json
 
@@ -39,70 +39,70 @@ class MrpRoutingWorkcenter(models.Model):
     _sql_constraints = [('routing_group_wc_uniq', 'unique(routing_id,group_id, workcenter_id)',
                          'Per Routing only has one unique Routing group per Work Center!')]
 
-    def _push_mrp_routing_workcenter(self, url, mrp_routing_workcenters):
-        for operation_id in mrp_routing_workcenters:
-            bom_id = self.env['mrp.bom'].search([('operation_ids', 'in', operation_id.ids)], limit=1)
-            if not bom_id:
-                continue
+    @api.one
+    def _push_mrp_routing_workcenter(self, url):
+        self.ensure_one()
+        operation_id = self
+        bom_id = self.env['mrp.bom'].search([('operation_ids', 'in', operation_id.ids)], limit=1)
+        if not bom_id:
+            return True
+        _points = []
+        for point in operation_id.operation_point_ids:
+            bom_line = self.env['mrp.bom.line'].search([('operation_id', '=', operation_id.id), ('operation_point_id', '=', point.id)])
+            qcp = self.env['quality.point'].search([('operation_id', '=', operation_id.id), ('bom_line_id', '=', bom_line.id)])
+            _points.append({
+                'sequence': point.sequence,
+                'group_sequence': point.group_sequence,
+                'offset_x': point.x_offset,
+                'offset_y': point.y_offset,
+                'max_redo_times': point.max_redo_times,
+                'tolerance_min': qcp.tolerance_min,
+                'tolerance_max': qcp.tolerance_max,
+                'tolerance_min_degree': qcp.tolerance_min_degree,
+                'tolerance_max_degree': qcp.tolerance_max_degree,
+                'consu_product_id': point.product_id.id if point.product_id.id else 0
+            })
 
-            _points = []
-            for point in operation_id.operation_point_ids:
-                bom_line = self.env['mrp.bom.line'].search([('operation_id', '=', operation_id.id), ('operation_point_id', '=', point.id)])
-                qcp = self.env['quality.point'].search([('operation_id', '=', operation_id.id), ('bom_line_id', '=', bom_line.id)])
-                _points.append({
-                    'sequence': point.sequence,
-                    'group_sequence': point.group_sequence,
-                    'offset_x': point.x_offset,
-                    'offset_y': point.y_offset,
-                    'max_redo_times': point.max_redo_times,
-                    'tolerance_min': qcp.tolerance_min,
-                    'tolerance_max': qcp.tolerance_max,
-                    'tolerance_min_degree': qcp.tolerance_min_degree,
-                    'tolerance_max_degree': qcp.tolerance_max_degree,
-                    'consu_product_id': point.product_id.id if point.product_id.id else 0
-                })
-
-            val = {
-                "id": operation_id.id,
-                "workcenter_id": operation_id.workcenter_id.id,
-                "job": int(operation_id.op_job_id.code) if operation_id.op_job_id else 0,
-                "max_op_time": operation_id.max_op_time,
-                "name": u"[{0}]{1}@{2}/{3}".format(operation_id.name, operation_id.group_id.code,
-                                                   operation_id.workcenter_id.name,
-                                                   operation_id.routing_id.name),
-                "img": u'data:{0};base64,{1}'.format('image/png',
-                                                     operation_id.worksheet_img) if operation_id.worksheet_img else "",
-                "product_id": bom_id.product_id.id if bom_id else 0,
-                "product_type": bom_id.product_id.vehicle_type_code if bom_id else "",
-                "workcenter_code": operation_id.workcenter_id.code if operation_id.workcenter_id else "",
-                'vehicleTypeImg': u'data:{0};base64,{1}'.format('image/png',
-                                                                bom_id.product_id.image_small) if bom_id.product_id.image_small else "",
-                "points": _points
-            }
-            try:
-                ret = Requests.put(url, data=json.dumps(val), headers={'Content-Type': 'application/json'})
-                if ret.status_code == 200:
-                    operation_id.write({'sync_download_time': fields.Datetime.now()})  ### 更新发送结果
-                    self.env.user.notify_info(u'下发工艺成功')
-            except ConnectionError:
-                self.env.user.notify_info(u'下发工艺失败')
-                break
-            except RequestException as e:
-                self.env.user.notify_info(u'下发工艺失败')
+        val = {
+            "id": operation_id.id,
+            "workcenter_id": operation_id.workcenter_id.id,
+            "job": int(operation_id.op_job_id.code) if operation_id.op_job_id else 0,
+            "max_op_time": operation_id.max_op_time,
+            "name": u"[{0}]{1}@{2}/{3}".format(operation_id.name, operation_id.group_id.code,
+                                               operation_id.workcenter_id.name,
+                                               operation_id.routing_id.name),
+            "img": u'data:{0};base64,{1}'.format('image/png',
+                                                 operation_id.worksheet_img) if operation_id.worksheet_img else "",
+            "product_id": bom_id.product_id.id if bom_id else 0,
+            "product_type": bom_id.product_id.vehicle_type_code if bom_id else "",
+            "workcenter_code": operation_id.workcenter_id.code if operation_id.workcenter_id else "",
+            'vehicleTypeImg': u'data:{0};base64,{1}'.format('image/png',
+                                                            bom_id.product_id.image_small) if bom_id.product_id.image_small else "",
+            "points": _points
+        }
+        try:
+            ret = Requests.put(url, data=json.dumps(val), headers={'Content-Type': 'application/json'}, timeout=1)
+            if ret.status_code == 200:
+                operation_id.write({'sync_download_time': fields.Datetime.now()})  ### 更新发送结果
+                self.env.user.notify_info(u'下发工艺成功')
+        except ConnectionError as e:
+            self.env.user.notify_warning(u'下发工艺失败, 错误原因:{0}'.format(e.message))
+        except RequestException as e:
+            self.env.user.notify_warning(u'下发工艺失败, 错误原因:{0}'.format(e.message))
         return True
 
     @api.multi
     def button_send_mrp_routing_workcenter(self):
-        self.ensure_one()
-        master = self.workcenter_id.masterpc_id if self.workcenter_id else None
-        if not master:
-            return True
-        connections = master.connection_ids.filtered(lambda r: r.protocol == 'http') if master.connection_ids else None
-        if not connections:
-            return True
-        url = ['http://{0}:{1}{2}'.format(connect.ip, connect.port, MASTER_WROKORDERS_API) for connect in connections][0]
+        for operation in self:
+            master = operation.workcenter_id.masterpc_id if operation.workcenter_id else None
+            if not master:
+                continue
+            connections = master.connection_ids.filtered(lambda r: r.protocol == 'http') if master.connection_ids else None
+            if not connections:
+                continue
+            url = ['http://{0}:{1}{2}'.format(connect.ip, connect.port, MASTER_WROKORDERS_API) for connect in connections][0]
 
-        self._push_mrp_routing_workcenter(url, self)
+            operation._push_mrp_routing_workcenter(url)
         return True
 
 
