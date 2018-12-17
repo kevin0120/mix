e0673cfc294a8b0038cf08cbfee5543bdd0dfa2c
linshenqi <linshenqi@outlook.com>
2018-12-17 15:19:07 +0800

    1. 完善了结果报表

--- changan_exhanced/report/tighting_result_report.py
+++ changan_exhanced/report/tighting_result_report.py
@@ -34,8 +34,8 @@ class ReportTightResultReport(models.AbstractModel):
         #         'lines': list(lines)
         #     })
 
-        query = "select tb.vin, json_agg(json_build_array(tb.control_date, tb.code, tb.measure_result)) " \
-                "from (select ors.vin as vin, ors.control_date as control_date, ors.measure_result as measure_result, cps.code as code from operation_result ors join controller_program cps on cps.id = ors.program_id where ors.control_date >= '%s' and ors.control_date <= '%s' order by ors.control_date) tb " \
+        query = "select tb.vin, json_agg(json_build_array(tb.control_date, tb.job, tb.measure_result)) " \
+                "from (select ors.vin as vin, ors.control_date as control_date, ors.measure_result as measure_result, ors.job as job from operation_result ors where ors.control_date >= '%s' and ors.control_date <= '%s' order by ors.control_date) tb " \
                 "group by tb.vin" % (
                     date_from,
                     date_to
