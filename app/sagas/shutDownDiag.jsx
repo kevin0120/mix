eacbe8d6ba93ca0e868b3a4c45f39eb36545b72f
cosine <cosine>
2019-01-14 17:43:35 +0800

    添加ak2

--- app/sagas/shutDownDiag.jsx
+++ app/sagas/shutDownDiag.jsx
@@ -56,7 +56,7 @@ function* confirmDiag(dType, data) {
         const op = yield select(getOperations);
         const { carID } = op;
         const state = yield select();
-        const enableAk2 = state.setting.operationSettings.manualFreestyle;
+        const enableAk2 = state.setting.operationSettings.enableAk2;
         if (enableAk2) {
           yield call(ak2);
         }
