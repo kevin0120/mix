7aa2434d53926431b341174e3cd6c633a7a43659
frank <frank@frankdeMacBook-Pro.local>
2019-01-27 15:20:01 +0800

    1.添加了字体 noto sans

--- app/common/jss/material-react-pro.jsx
+++ app/common/jss/material-react-pro.jsx
@@ -80,7 +80,7 @@ const card = {
 };
 
 const defaultFont = {
-  fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  fontFamily: '"Noto Sans","Noto Sans SC","Roboto", "Helvetica", "Arial", sans-serif',
   fontWeight: '300',
   lineHeight: '1.5em'
 };
--- app/index.js
+++ app/index.js
@@ -33,6 +33,7 @@ const theme = createMuiTheme({
     useNextVariants: true,
     // Use the system font instead of the default Roboto font.
     fontFamily: [
+      'Noto Sans',
       'Noto Sans SC',
       '"Segoe UI"',
       'Roboto',
