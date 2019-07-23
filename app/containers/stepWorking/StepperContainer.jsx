956074a357c3919a1703360bb5c038d75bd14f53
cosine <cosine>
2019-07-23 14:33:51 +0800

    fix StepperContainer

--- app/containers/stepWorking/StepperContainer.jsx
+++ app/containers/stepWorking/StepperContainer.jsx
@@ -1,5 +1,5 @@
 import { makeStyles, StepContent, Typography } from '@material-ui/core';
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
@@ -11,12 +11,24 @@ import styles from './styles';
 import * as orderSelectors from '../../modules/order/selector';
 import { orderActions } from '../../modules/order/action';
 
-const StepperContainer = () => {
-  const dispatch = useDispatch();
-  const classes = makeStyles(styles.stepperContainer)();
-  const steps = useSelector(state => orderSelectors.orderSteps(state.order) || []);
-  const viewingIndex = useSelector(state => orderSelectors.viewingIndex(state.order) || 0);
+const mapState = (state, props) => ({
+  ...props,
+  steps: orderSelectors.orderSteps(state.order) || [],
+  viewingIndex: orderSelectors.viewingIndex(state.order) || 0
+});
 
+const mapDispatch = {
+  jumpTo: orderActions.jumpToStep
+};
+
+type StepperLayoutProps = {
+  steps: [],
+  viewingIndex: {},
+  jumpTo: () => {}
+};
+
+const StepperContainer = ({ steps, viewingIndex, jumpTo }: StepperLayoutProps) => {
+  const classes = makeStyles(styles.stepper)();
   return (
     <Stepper
       nonLinear
@@ -33,7 +45,7 @@ const StepperContainer = () => {
           <Step key={s.name} {...stepProps}>
             <StepButton
               completed={s.status === ORDER_STEP_STATUS.FINISHED}
-              onClick={() => dispatch(orderActions.jumpToStep(id))}
+              onClick={() => jumpTo(id)}
               className={classes.stepButton}
             >
               <StepLabel {...labelProps}>{s.name}</StepLabel>
@@ -48,4 +60,4 @@ const StepperContainer = () => {
   );
 };
 
-export default StepperContainer;
+export default connect(mapState, mapDispatch)(StepperContainer);
