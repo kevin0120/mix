91e36d63f15b45a7d4e2652966a4b23ed0ea3200
frank <frank@frankdeMacBook-Pro.local>
2019-10-20 12:51:10 +0800

    [FIX] 1. ctx 使用更优雅的方法创建

--- spc/models/quality.py
+++ spc/models/quality.py
@@ -9,20 +9,18 @@ class QualityPoint(models.Model):
     @api.multi
     def action_see_spc_control(self):
         action = self.env.ref('spc.quality_check_action_spc').read()[0]
+        ctx = dict(self._context, search_default_qcp_id=self.id)
         action.update({
-            'context': {
-                'search_default_qcp_id': self.id
-            }
+            'context': ctx
         })
         return action
 
     @api.multi
     def action_see_result(self):
         action = self.env.ref('spc.operation_result_action_main').read()[0]
+        ctx = dict(self._context, search_default_qcp_id=self.id)
         action.update({
-            'context': {
-                'search_default_qcp_id': self.id
-            }
+            'context': ctx
         })
         return action
 
@@ -31,10 +29,10 @@ class QualityCheck(models.Model):
     _inherit = "sa.quality.check"
 
     @api.depends('result_ids')
-    def _get_operation_results(self):
+    def _compute_operation_results(self):
         for check in self:
             check.result_count = len(check.result_ids)
 
     result_ids = fields.One2many('operation.result', 'quality_check_id', string='Operation Results')
 
-    result_count = fields.Integer(string='Operation Result Count', compute=_get_operation_results)
+    result_count = fields.Integer(string='Operation Result Count', compute=_compute_operation_results)
