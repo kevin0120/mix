15e98227e244b7ede03ab5384ec4de352981f893
kevin <jingwei1224@163.com>
2019-12-04 16:14:16 +0800

    结果上传aiis bug修复

--- services/storage/service.go
+++ services/storage/service.go
@@ -180,7 +180,7 @@ func (s *Service) FindUnuploadResults(result_upload bool, result []string) ([]Re
 func (s *Service) ListUnuploadResults() ([]Results, error) {
 	var results []Results
 
-	ss := s.eng.Alias("r").Where("r.has_upload = ?", false).And("r.curve_file != ?", "")
+	ss := s.eng.Alias("r").Where("r.has_upload = ?", false)
 
 	e := ss.Find(&results)
 
