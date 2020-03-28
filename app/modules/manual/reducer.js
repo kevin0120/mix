f8cc1b42c794f2dd2a620de101c619e1edf73e9b
kevin <jingwei1224@163.com>
2020-03-28 15:45:08 +0800

    手动输入在进入界面后就将扫码触发工单功能取消

--- app/modules/manual/reducer.js
+++ app/modules/manual/reducer.js
@@ -1,42 +1,46 @@
 // @flow
-import { MANUAL,TIMELINE_STORY} from './action';
+import { MANUAL, TIMELINE_STORY } from './action';
 import type { StateType, Action } from './types';
 
 const configs = {
   counter: 2,
   scanner: '',
-  controllerSN:'',
+  controllerSN: '',
   tool: '',
   pset: 0,
-  result:[],
-  timeline:[]
-}
+  result: [],
+  timeline: []
+};
 
-export default function (state: StateType = configs, action: Action) {
+export default function(state: StateType = configs, action: Action) {
   switch (action.type) {
     case MANUAL.SETDATA:
-      return { ...state,controllerSN:action.ControllerSN,tool:action.tool, pset:action.pset };
+      return {
+        ...state,
+        controllerSN: action.ControllerSN,
+        tool: action.tool,
+        pset: action.pset
+      };
     case MANUAL.NEWCANNERDATA:
       return { ...state, scanner: action.scanner };
     case MANUAL.GETRESULT:
       return { ...state, result: state.result.concat(action.result) };
-    case MANUAL.START:
-      return { ...state, working: true };
-    case MANUAL.CLOSE:
-      return { ...state, working: false};
+    // case MANUAL.START:
+    //   return { ...state, working: true };
+    // case MANUAL.CLOSE:
+    //   return { ...state, working: false};
     case MANUAL.DIDMOUNT:
-      return { ...state, result: []};
+      return { ...state, result: [], working: true };
     case TIMELINE_STORY.NEW:
-      return { ...state, timeline: NewStory(state.timeline, action.story)};
+      return { ...state, timeline: NewStory(state.timeline, action.story) };
     case TIMELINE_STORY.CLEAR:
-      return { ...state, timeline: ClearStory()};
+      return { ...state, timeline: ClearStory(), working: false };
     case MANUAL.RESULTINPUT:
-      return { ...state, resultIn: action.resultIn};
+      return { ...state, resultIn: action.resultIn };
     default:
       return state;
   }
 }
-
 
 export function NewStory(state, story) {
   return [story, ...state];
